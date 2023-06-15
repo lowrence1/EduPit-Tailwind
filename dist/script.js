936cb950b91950df0d62a4c5425ca0682ec5dc8d
aldrin1122 <0318-1190@lspu.edu.ph>
2023-06-15 15:34:42 +0800

    updated

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,26 @@
+const headerNav = document.querySelectorAll(".drop__nav h4");
+headerNav.forEach((hNav) => {
+  hNav.addEventListener("mousemove", () => {
+    hNav.nextElementSibling.classList.add("drop");
+  });
+  hNav.addEventListener("mouseout", () => {
+    hNav.nextElementSibling.classList.remove("drop");
+  });
+});
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerBot = document.querySelector(".header__nav nav");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerBot.classList.toggle("open");
+});
+
+const dropNav = document.querySelectorAll(".drop__nav h4");
+dropNav.forEach((dropN) => {
+  dropN.addEventListener("click", () => {
+    dropN.nextElementSibling.classList.toggle("open");
+    dropN.querySelector(".drop__toggle").classList.toggle("open");
+  });
 });
 
 const faqs = document.querySelectorAll(".header__item h4");
@@ -12,3 +30,7 @@ faqs.forEach((drop) => {
     drop.querySelector("i").classList.toggle("open");
   });
 });
+
+
+
+
